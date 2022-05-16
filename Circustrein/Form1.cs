2db923aac75697da8809f4fa73c4f6d55247cbf1
davey van den eijnde <davey2206@LIVE.NL>
2022-05-16 11:42:12 +0200

    added train :)

--- Circustrein/Form1.cs
+++ Circustrein/Form1.cs
@@ -37,18 +37,34 @@ namespace Circustrein
             Train train = new Train(animals);
             train.AddWagons();
 
-            int x = 0;
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Location = new System.Drawing.Point(0, 0);
+            pictureBox.Size = new System.Drawing.Size(50, 100);
+            pictureBox.Image = Properties.Resources.train;
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            TrainPanel.Controls.Add(pictureBox);
+
+            int x = 60;
             int y = 0;
             foreach (var wagon in train.Wagons)
             {
+                Label lb = new Label();
+                lb.Size = new System.Drawing.Size(8, 100);
+                lb.Text = "_";
+                lb.Location = new System.Drawing.Point((x - 10), (y + 40));
+
                 ListBox listBox = new ListBox();
                 listBox.Location = new System.Drawing.Point(x, y);
                 listBox.Size = new System.Drawing.Size(50, 100);
+
                 foreach (var animal in wagon.Animals)
                 {
                     listBox.Items.Add(animal.Point.ToString());
                 }
+
                 TrainPanel.Controls.Add(listBox);
+                TrainPanel.Controls.Add(lb);
+
                 x = x + 60;
                 if (x > (TrainPanel.Size.Width - 125))
                 {
